8e3aa0dd42755b3c3625df2a2ba2d16e45a7e7c2
JeanC4rlo <jeancs.santos1@gmail.com>
2025-10-12 16:18:24 -0300

    Envio do Dashboard inicial

---
+++ scripts/homeSections.js
@@ -0,0 +1,46 @@
+function highlightOption(sectionId) {
+    const buttons = document.querySelectorAll("nav button");
+
+    buttons.forEach(button => {
+        if (button.dataset.section === sectionId)
+            button.classList.add("ativo");
+        else
+            button.classList.remove("ativo");
+    })
+}
+
+function openSection(sectionId) {
+    const sections = document.querySelectorAll("main section");
+
+    sections.forEach(section => {
+        if (section.id === sectionId)
+            section.classList.add("ativo");
+        else
+            section.classList.remove("ativo");
+    });
+
+    localStorage.setItem("secaoAtiva", sectionId);
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let sectionButtons = document.querySelectorAll("nav button");
+    sectionButtons.forEach(button => {
+        button.addEventListener("click", (e) => {
+            e.preventDefault();
+            const targetSection = button.dataset.section;
+            highlightOption(targetSection);
+            openSection(targetSection);
+        });
+    });
+    
+    const ultimaSecao = localStorage.getItem("secaoAtiva");
+
+    if (ultimaSecao) {
+        highlightOption(ultimaSecao);
+        openSection(ultimaSecao);
+    }  
+    else {
+        highlightOption("boletim")
+        openSection("boletim");
+    }
+});
---
+++ scripts/tabelaDinamica.js
@@ -0,0 +1,77 @@
+const notasBD = [
+    {
+        "ano": "2023",
+        "materias": [["Português", "2", "2", "2", "2"], ["Matemática", "2", "2", "2", "2"]],
+        "faltas": ["0", "0"],
+        "final": ["8", "8"],
+        "situacao": ["REPRV", "REPRV"]
+    },
+    {
+        "ano": "2024",
+        "materias": [["Português", "10", "10", "10", "10"], ["Matemática", "10", "10", "10", "10"]],
+        "faltas": ["0", "0"],
+        "final": ["8", "8"],
+        "situacao": ["REPRV", "REPRV"]
+    },
+    {
+        "ano": "2025",
+        "materias": [["Português", "20", "20", "20", "20"], ["Matemática", "20", "20", "20", "20"]],
+        "faltas": ["0", "0"],
+        "final": ["8", "8"],
+        "situacao": ["REPRV", "REPRV"]
+    }
+];
+
+const botaoBoletim = document.querySelector("#botaoAcessoBoletim")
+const botaoEsq = document.querySelector("#botaoEsq");
+const botaoDir = document.querySelector("#botaoDir");
+let idx = 0;
+
+botaoBoletim.addEventListener('click', () => preencherTabela());
+botaoEsq.addEventListener('click', () => passarAno(-1));
+botaoDir.addEventListener('click', () => passarAno(1));
+
+function passarAno(incremento) {
+    let novoIdx = idx + incremento;
+
+    if(novoIdx <= 2 && novoIdx >= 0) {
+        idx = novoIdx;
+    }
+    
+    preencherTabela();
+}
+
+function preencherTabela() {
+    const tabela = document.getElementById("tabelaBoletim");
+    const tbody = tabela.querySelector("tbody");
+    const ano = tabela.querySelector("div > p");
+    let notas = notasBD[idx];
+
+    tbody.innerHTML = "";
+    
+    ano.textContent = notas.ano;
+
+    for(let i = 0; i < notas.materias.length; i++) {
+        const tr = document.createElement("tr");
+
+        notas.materias[i].forEach(v => {
+            const td = document.createElement("td");
+            td.textContent = v;
+            tr.appendChild(td);
+        })
+
+        const tdFaltas = document.createElement("td");
+        tdFaltas.textContent = notas.faltas[i];
+        tr.appendChild(tdFaltas);
+
+        const tdNotaFinal = document.createElement("td");
+        tdNotaFinal.textContent = notas.final[i]; 
+        tr.appendChild(tdNotaFinal);
+
+        const tdSituacao = document.createElement("td");
+        tdSituacao.textContent = notas.situacao[i];
+        tr.appendChild(tdSituacao);
+
+        tbody.appendChild(tr);
+    }
+}
